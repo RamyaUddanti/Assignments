5754c6eeb868ed6b654aede647ab651d73eadeb2
RamyaUddanti <139635334+RamyaUddanti@users.noreply.github.com>
2023-08-23 20:29:58 +0530

    Update Fun with Lists and Tuples.py

--- Fun with Lists and Tuples.py
+++ Fun with Lists and Tuples.py
@@ -1,6 +1,6 @@
-def last(n): return n[-1]
-
-def sort_list_last(tuples):
-  return sorted(tuples, key=last)
-
-print(sort_list_last([(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]))
+a=[(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+def last(a):
+    return a[-1:]
+def list_sorted(a):
+    return sorted(a,key=last)
+print(list_sorted(a))
